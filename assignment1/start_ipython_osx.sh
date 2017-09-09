b875a2937168781dd9d82295f87a5eeb471374a1
Yufan Liu <yliu12@cluster-001.lenovo-hkecs.com>
2017-09-09 19:09:47 +0800

    A1 - KNN

---
+++ assignment1/start_ipython_osx.sh
@@ -0,0 +1,5 @@
+# Assume the virtualenv is called .env
+
+# cp frameworkpython .env/bin
+cp frameworkpython /Users/patrick/anaconda/envs/py36/bin/
+/Users/patrick/anaconda/envs/py36/bin/frameworkpython -m IPython notebook
